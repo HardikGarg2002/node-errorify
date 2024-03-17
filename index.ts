0a4d653ebf9dc5bbc0586a69d0fef5c5ced5949c
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-03-18 02:20:00 +0530

    exporting all from index

--- index.ts
+++ index.ts
@@ -1,3 +1,12 @@
 import APIError from "./src/errors/ApiErrors";
+import ValidationErrors, { IFieldError } from "./src/errors/ValidationErrors";
+import asyncHandler from "./src/middleware/asyncHandler";
+import globalErrorHandler from "./src/middleware/globalErrorHandler";
 
-export { APIError };
+export {
+  APIError,
+  ValidationErrors,
+  globalErrorHandler,
+  asyncHandler,
+  IFieldError,
+};
