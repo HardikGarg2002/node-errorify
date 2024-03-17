ad3226791268495f29e77961524b8d09a94243a9
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-03-18 02:20:00 +0530

    feat: validation error class with error on multiple input fields

---
+++ src/errors/ValidationErrors.ts
@@ -0,0 +1,39 @@
+export interface IFieldError {
+  field: string;
+  message: string;
+  errorCode: string;
+}
+
+export default class ValidationErrors extends Error {
+  public fieldErrors: IFieldError[];
+  public status: number;
+
+  constructor(message = "Validation failed") {
+    super(message);
+    this.status = 400;
+    this.fieldErrors = [];
+    Object.setPrototypeOf(this, new.target.prototype);
+    this.name = this.constructor.name;
+  }
+
+  addError(field: string, message: string, errorCode: string) {
+    this.fieldErrors.push({ field, message, errorCode });
+  }
+
+  fromValidationLibrary(errors: any) {
+    errors.forEach((err: any) => {
+      this.addError(err.path || "unknown", err.message, "VALIDATION_ERROR");
+    });
+  }
+
+  toJSON() {
+    return {
+      message: this.message,
+      errors: this.fieldErrors,
+    };
+  }
+
+  hasErrors() {
+    return this.fieldErrors.length > 0;
+  }
+}
